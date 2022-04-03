381cefac15547e583188448e568226f619b952c5
LemzaSergey <55410819+LemzaSergey@users.noreply.github.com>
2022-04-04 01:39:12 +0300

    create_users

--- api/objects/users.php
+++ api/objects/users.php
@@ -1,5 +1,6 @@
 <?php
-class Users {
+class Users
+{
 
     // подключение к базе данных и таблице 'users'
     private $conn;
@@ -12,13 +13,57 @@ class Users {
     public $phone;
     public $password;
     public $payment_card;
-    
+
 
     // конструктор для соединения с базой данных
-    public function __construct($db){
+    public function __construct($db)
+    {
         $this->conn = $db;
     }
 
-    // здесь будет метод read()
+    // Создание нового пользователя
+    function create()
+    {
+
+        // Вставляем запрос
+        $query = "INSERT INTO " . $this->table_name . "
+            SET
+                name = :name,
+                mail = :mail,
+                phone = :phone,
+                password = :password,
+                payment_card = :payment_card";
+
+        // подготовка запроса
+        $stmt = $this->conn->prepare($query);
+
+        // инъекция
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->mail = htmlspecialchars(strip_tags($this->mail));
+        $this->phone = htmlspecialchars(strip_tags($this->phone));
+        $this->password = htmlspecialchars(strip_tags($this->password));
+        $this->payment_card = htmlspecialchars(strip_tags($this->payment_card));
+
+        // привязываем значения
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':mail', $this->mail);
+        $stmt->bindParam(':phone', $this->phone);
+        $stmt->bindParam(':payment_card', $this->payment_card);
+
+        // для защиты пароля
+        // хешируем пароль перед сохранением в базу данных
+        $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
+        $stmt->bindParam(':password', $password_hash);
+
+        // Выполняем запрос
+        // Если выполнение успешно, то информация о пользователе будет сохранена в базе данных
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // здесь будет метод emailExists() 
 }
 ?>
