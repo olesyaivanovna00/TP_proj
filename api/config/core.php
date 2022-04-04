e8dd5f89d6f9d224e713c3657feeaadd4af28a1d
LemzaSergey <55410819+LemzaSergey@users.noreply.github.com>
2022-04-04 19:49:02 +0300

    add jwt to login

--- api/config/core.php
+++ api/config/core.php
@@ -4,4 +4,14 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // URL домашней страницы
-$home_url="http://localhost/api/";
+$home_url = "http://localhost/api/";
+
+// установить часовой пояс по умолчанию
+date_default_timezone_set('Europe/Moscow');
+
+// переменные, используемые для JWT
+$key = "secret_key";
+$iss = "SOK";
+$sub = "auth";
+$aud = "http://SOK/";
+$iat = 1649084991;
